f7eb7c2c5ad1ae029d096a9af6e069bd674c7d56
AchabNassim <achab.nassim.solicode@gmail.com>
2023-07-11 17:34:04 +0100

    needs some more styling, lacks delete all and check task

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const ul = document.getElementById("list");
 const submitButton = document.getElementById("submitButton");
 const inputField = document.getElementById("todoInput");
 const paragError = document.getElementById("paragError");
+let update = false;
 
-function displayError() {
+function displayError(errorMessage) {
+    paragError.textContent = errorMessage;
     submitButton.className = "error";
     inputField.className = "error";
     paragError.style.visibility = "visible";
@@ -13,14 +15,47 @@ function displayError() {
         inputField.className = "input";
         submitButton.className = "input";
         paragError.style.visibility = "hidden";
-    }, 1500);
+    }, 2000);
 }
 
 function createLiElement(text) {
     const li = document.createElement("li");
+    const update = document.createElement("a");
+    const del = document.createElement("a");
+    update.href = "#";
+    update.className = "fa-solid fa-square-pen";
+    del.href = "#";
+    del.className = "fa-solid fa-trash";
     li.className = "listElement";
     li.textContent = text;
+    li.appendChild(update);
+    li.appendChild(del);
     return (li);
+}
+
+function modify (e) {
+    let node;
+    if (update == true)
+    {
+        displayError("Please finish modifying first before proceeding!");
+        return ;
+    }
+    if (e.target.classList.contains('fa-square-pen')) {
+        node = e.target.parentElement;
+        update = true;
+        node.classList.add("liUpdate");
+        inputField.value = node.innerText;
+        submitButton.value = "update!";
+        inputField.focus();
+    }
+}
+
+function deleleteLi(e) {
+    let node;
+    if (e.target.classList.contains('fa-trash')) {
+        node = e.target.parentElement;
+        node.remove();
+    }
 }
 
 function processInput(e) {
@@ -28,10 +63,21 @@ function processInput(e) {
     let inputValue = inputField.value;
     inputField.value = "";
     if (inputValue.trim().length === 0 || inputValue === undefined) {
-        displayError();
+        displayError("Error, please enter something!");
         return ;
     }
-    ul.appendChild(createLiElement(inputValue));
+    if (update === false) {
+        ul.appendChild(createLiElement(inputValue));
+    }
+    else {
+        update = false;
+        submitButton.value = "save!";
+        const element = document.querySelector(".liUpdate");
+        element.childNodes[0].textContent = inputValue;
+        element.classList.remove("liUpdate");
+    }
 }
 
 submitButton.addEventListener("click", processInput);
+ul.addEventListener("click", modify);
+ul.addEventListener("click", deleleteLi);
